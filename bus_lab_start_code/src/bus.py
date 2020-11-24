ef533f7a6caf2a8d335d93a2d0e077864e034814
Andrew Wood <andrewwood90@hotmail.com>
2020-11-24 15:20:32 +0000

    First Extension finished

---
+++ bus_lab_start_code/src/bus.py
@@ -0,0 +1,26 @@
+class Bus:
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
+        self.destination = destination
+        #self.drive_method = "Brum brum"
+        self.passengers = []
+
+
+    def drive(self):
+        return "Brum brum"
+
+    def passenger_count(self):
+        return len(self.passengers)
+
+    def pick_up(self, passenger):
+        self.passengers.append(passenger)
+
+    def drop_off(self, passenger):
+        self.passengers.remove(passenger)
+
+    def empty(self):
+        self.passengers.clear()
+
+    def pick_up_from_stop(self, bus_stop):
+        self.passengers.extend(bus_stop.queue)
+        bus_stop.queue.clear()
---
+++ bus_lab_start_code/src/person.py
@@ -0,0 +1,13 @@
+
+
+class Person:
+    def __init__(self, name, age, destination):
+        self.name = name
+        self.age = age
+        self.destination = destination
+
+
+
+king_street = Bus
+
+joe = Person("Joe", 40, )
